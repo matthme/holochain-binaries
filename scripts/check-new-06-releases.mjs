61e6cac9c56dbc626bae0299a3d46b82bb6ecd3e
Matthias <tuttiacc@hotmail.com>
2025-06-18 11:35:58 +0200

    add check for 0.6 releases

--- scripts/check-new-06-releases.mjs
+++ scripts/check-new-06-releases.mjs
@@ -13,7 +13,7 @@ const holochainReleasesOptions = {
 const binariesTagsOptions = {
   hostname: 'api.github.com',
   port: 443,
-  path: '/repos/matthme/holochain-binaries/tags',
+  path: '/repos/matthme/holochain-binaries/git/refs/tags',
   method: 'GET',
   headers: {
     'User-Agent': 'Holochain Binaries'
@@ -22,17 +22,17 @@ const binariesTagsOptions = {
 
 getAndDo(holochainReleasesOptions, (data) => {
   const allReleases = JSON.parse(data);
-  // filter by releases that have been published in the last 10 hours
-  const recentReleases = allReleases.filter((release) => Date.now() - (new Date(release.published_at)).getTime() < 36000000);
+  // filter by releases that have been published in the last 2 weeks
+  const recentReleases = allReleases.filter((release) => Date.now() - (new Date(release.published_at)).getTime() < 1209600000);
   getAndDo(binariesTagsOptions, (data) => {
     const binaryTags = JSON.parse(data);
-    // Check for holochain 0.3 releases that have not yet a tag in the holochain-binaries repo
-    const binaryTagHcVersions = binaryTags.map((tag) => tag.name.replace('-binaries', ''));
+    // Check for holochain 0.6 releases that have not yet a tag in the holochain-binaries repo
+    const binaryTagHcVersions = binaryTags.map((tag) => tag.ref.replace('refs/tags/', '').replace('-binaries', ''));
     const unbuiltReleases = recentReleases.map((release) => release.tag_name).filter((releaseName) => !binaryTagHcVersions.includes(releaseName));
-    const unbuilt03Releases = unbuiltReleases.filter((tagName) => tagName.startsWith('holochain-0.3'));
+    const unbuilt05Releases = unbuiltReleases.filter((tagName) => tagName.startsWith('holochain-0.6'));
     // We assume that there is only one new release without tag
-    if (unbuilt03Releases.length > 0) {
-      console.log(unbuilt03Releases[0].replace('holochain-', ''));
+    if (unbuilt05Releases.length > 0) {
+      console.log(unbuilt05Releases[0].replace('holochain-', ''));
     }
 
   })
